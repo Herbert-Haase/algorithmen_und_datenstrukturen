d2c36005debf97b3b0142735cacc6a62a34e4bbf
Herbert-Haase <herberthaase02@gmx.de>
2025-11-22 22:15:54 +0100

    delete scala files

--- build.sbt
+++
@@ -1,4 +0,0 @@
-scalaVersion := "3.5.0"
-
-// Ensure proper source directories
-Compile / scalaSource := baseDirectory.value / "src" / "main" / "scala"
--- src/main/scala/ex6/Merge.scala
+++
@@ -1,7 +0,0 @@
-package ex6
-
-import scala.reflect.ClassTag
-
-class Merge(a:Array[Int]) extends Sortieren(a):
-
-    override def sort(): Array[Int] = a
