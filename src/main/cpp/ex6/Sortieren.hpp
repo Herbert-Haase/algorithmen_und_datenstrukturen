ab9fee63b8e911f4090614ca72fa084d42902b5f
Herbert-Haase <herberthaase02@gmx.de>
2025-12-19 22:50:11 +0100

    refactor ex6

--- src/main/cpp/ex6/Sortieren.hpp
+++ src/main/cpp/ex6/Sortieren.hpp
@@ -1,7 +1,6 @@
 #ifndef SORTIEREN_H
 #define SORTIEREN_H
 
-#include <cstdlib>
 #include <iostream>
 #include <memory>
 #include <span>
@@ -17,16 +16,20 @@ protected:
 
 public:
   Sortieren(std::span<int> arr);
+  virtual ~Sortieren() = default;
+
   Sortieren(const Sortieren &) = delete;
   Sortieren &operator=(const Sortieren &) = delete;
   Sortieren(Sortieren &&) = delete;
   Sortieren &operator=(Sortieren &&) = delete;
-  virtual ~Sortieren() = default;
+
   friend std::ostream &operator<<(std::ostream &os, const Sortieren &s);
   int &operator[](int index);
 
-  virtual void countAndReset();
-  int get(unsigned idx);
+  void countAndReset();
+  
+  bool less(int lhs, int rhs);
+  
   void swap(int i, int j);
 
   virtual void sort() = 0;
