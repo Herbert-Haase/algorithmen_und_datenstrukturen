a0e18d8572e63b261b00541d88f6aa13b53411d3
Atom <atomcraft.4u@web.de>
2026-01-26 23:14:03 +0100

    scala: Main: better overview

--- src/main/scala/graph/Main.scala
+++ src/main/scala/graph/Main.scala
@@ -4,6 +4,24 @@ import graph.topologicSort.TopologicSort as Topo
 import graph.shortestPath.Dijkstra as Dijk
 
 @main def main(): Unit =
+  val g = exampleTopo
+  println(Topo.isSortable(g.toVector))
+  println(Topo.sortGraph(g.toVector).get.mkString(", "))
+
+  val d = exampleDijkstra
+  val (distance, lastNode) = Dijk.shortestPath(d.toVector, 0)
+  println(f"Distances: $distance")
+  println(f"last Node: $lastNode")
+
+case class GraphNode(key: Int, neighbours: List[Int] = List()):
+  override def toString: String = key.toString
+
+case class DirectedWeightedNode(key: Int, neighbours: List[WeightEdge]):
+  override def toString: String = key.toString
+
+case class WeightEdge(target: Int, cost: Int)
+
+def exampleTopo: Array[GraphNode] =
   val g: Array[GraphNode] = Array.ofDim(13)
   addNode(g, 11)()
   addNode(g, 10)(11)
@@ -18,9 +36,9 @@ import graph.shortestPath.Dijkstra as Dijk
   addNode(g, 2)(3)
   addNode(g, 1)(2, 3)
   addNode(g, 0)(1)
-  println(Topo.isSortable(g.toVector))
-  println(Topo.sortGraph(g.toVector).get.mkString(", "))
+  g
 
+def exampleDijkstra =
   val d: Array[DirectedWeightedNode] = Array.ofDim(9)
   addNode(d, 0)((1, 1), (3, 2), (2, 6), (4, 2))
   addNode(d, 1)((0, 1), (2, 4), (5, 5))
@@ -31,9 +49,7 @@ import graph.shortestPath.Dijkstra as Dijk
   addNode(d, 6)((4, 1), (7, 1))
   addNode(d, 7)((6, 1), (2, 8), (8, 1))
   addNode(d, 8)((5, 7), (7, 1))
-  val (distance, lastNode) = Dijk.shortestPath(d.toVector, 0)
-  println(f"Distances: $distance")
-  println(f"last Node: $lastNode")
+  d
 
 def addNode(arr: Array[GraphNode], key: Int)(neighbours: Int*) =
   arr(key) = GraphNode(key, neighbours.toList)
@@ -42,10 +58,3 @@ def addNode(arr: Array[GraphNode], key: Int)(neighbours: Int*) =
 def addNode(arr: Array[DirectedWeightedNode], key: Int)(neighbours: (Int, Int)*) =
   arr(key) = DirectedWeightedNode(key, neighbours.map(a => WeightEdge(a._1, a._2)).toList)
   arr
-
-case class GraphNode(key: Int, neighbours: List[Int] = List()):
-  override def toString: String = key.toString
-
-case class DirectedWeightedNode(key: Int, neighbours: List[WeightEdge])
-
-case class WeightEdge(target: Int, cost: Int)
